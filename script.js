d1867d0d2629098a6958a4217a5c17e99d8f2b6e
Duncan <wangari.dun@gmail.com>
2022-01-18 19:29:53 +0300

    Add winner selector function

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
+//HOW TO PLAY ROCK PAPER SCISSORS
+
+    //ROCK BEATS SCISSORS
+    //SCISSORS BEATS PAPER
+    //PAPER BEATS ROCK
+
+
+const score = {
+    player: 0,
+    computer: 0
+};
+
+
+//Main play function
+function game(){
+    let playerSelection = prompt("What is your choice? ", "Rock, Paper or Scissors?");
+    playerSelection = playerSelection.toUpperCase();
+    let computerSelection = computerPlay();
+    computerSelection = computerSelection.toUpperCase();
+
+    let winner = winnerSelector(playerSelection, computerSelection);
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(winner)
+}
+
 //Computer play. It returns Rock Paper or Scissors
 function computerPlay(){
-    let choices = [
+    let randomChoices = [
         "Rock",
         "Paper",
         "Scissors"
     ];
-    choices = choices[Math.floor(Math.random()*choices.length)];
-    return choices;
+    randomChoices = randomChoices[Math.floor(Math.random()*randomChoices.length)];
+    return randomChoices;
 
 }
 
+//Function that determines the winner
+function winnerSelector(plyS, cmpS){
+    if(plyS === cmpS){
+        console.log("Thats a draw!");        
+    }else if (plyS === "ROCK"){
+        if (cmpS === "SCISSORS"){
+            return ("You win! Rock beats Scissors!");
+        }else {
+            return ("You loose! Paper beats Rock!");
+        }
+    }else if (plyS === "SCISSORS"){
+        if (cmpS === "PAPER"){
+            return ("You win! Scissors beats Paper!");
+        }else {
+            return ("You loose! Paper beats Rock!");
+        }
+    }else {
+        if (cmpS === "ROCK"){
+            return ("You win! Paper beats Rock!");
+        }else {
+            return ("You loose! Scissors beats Paper!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+game();
